ff9d6ce51c4e9fd4b6f97e38f0c40c32f203bfb9
Sam Dilullo <samdilullo72@gmail.com>
2025-02-06 17:37:29 -0800

    updated database informtion

--- DDL.sql
+++ DDL.sql
@@ -51,48 +51,65 @@ CREATE OR REPLACE TABLE Moves (
 --intersection table
 CREATE OR REPLACE TABLE Pokemon_Moves ( 
     pokemon_moves_id int NOT NULL AUTO_INCREMENT,
-    move_id int NOT NULL,
     pokemon_id int NOT NULL,
+    move_id int NOT NULL,
     PRIMARY KEY (pokemon_moves_id),
     FOREIGN KEY (move_id) REFERENCES Moves(move_id),
     FOREIGN KEY (pokemon_id) REFERENCES Pokemon(pokemon_id)
 );
-
+--data
 INSERT INTO Pokemon (pokemon_name, pokemon_type, health)
-VALUES ("Cat", "Normal", 10),
-("Dog", "Fighting", 11),
-("Rat", "Darkness", 5),
-("Bird", "Flying", 7);
+VALUES ("Farfetch'd", "Fighting", 80),
+("Bulbasaur", "Grass", 90),
+("Charmander", "Fire", 95);
+
 
 INSERT INTO Trainers (items_held, battle_record)
 VALUES ("Potion", 0),
 ("Egg", 1),
 ("Potion", 3),
-("Bandage", 5);
+("Revive", 5);
 
 INSERT INTO Moves (move_type, move_dmg, move_name)
-VALUES("fire", 10, "fireball"),
-("water", 5, "water gun"),
-("earth", 11, "rock"),
-("air", 4, "gust");
+VALUES("Fighting", 10, "Rock Smash"),
+("Fire", 12, "Fireball"),
+("Grass", 10, "Vine Whip");
 
 INSERT INTO Pokemon_Trainers(pokemon_id, trainer_id)
 VALUES(1,1),
-(2,1),
+(2,2),
+(3,3);
+
+INSERT INTO Pokemon_Moves(pokemon_id, move_id)
+VALUES(1,1),
+(2,3),
 (3,2);
 
-SELECT * FROM Pokemon;
+INSERT INTO Gyms(gym_leader_id)
+VALUES(1),
+(2),
+(3);
 
-SELECT * FROM Trainers;
 
-SELECT * FROM Moves;
+--SELECT * FROM Pokemon;
+
+--SELECT * FROM Trainers;
+
+--SELECT * FROM Moves;
 
 --displays trainers team based on trainer id. 
-SELECT Pokemon.pokemon_name, Trainers.items_held, Trainers.battle_record
-FROM Pokemon_Trainers
-INNER JOIN Pokemon ON Pokemon_Trainers.pokemon_id = Pokemon.pokemon_id
-INNER JOIN Trainers ON Pokemon_Trainers.trainer_id = Trainers.trainer_id
-WHERE Trainers.trainer_id = 1; 
+--SELECT Pokemon.pokemon_name, Trainers.items_held, Trainers.battle_record
+--FROM Pokemon_Trainers
+--INNER JOIN Pokemon ON Pokemon_Trainers.pokemon_id = Pokemon.pokemon_id
+--INNER JOIN Trainers ON Pokemon_Trainers.trainer_id = Trainers.trainer_id
+--WHERE Trainers.trainer_id = 1; --id num correlates to what trainer is selected
+
+--displays pokemon and their respective moves
+--SELECT Pokemon.pokemon_name, Moves.move_name, Moves.move_type, Moves.move_dmg
+--FROM Pokemon_Moves
+--INNER JOIN Pokemon ON Pokemon_Moves.pokemon_id = Pokemon.pokemon_id
+--INNER JOIN Moves ON Pokemon_Moves.move_id = Moves.move_id
+--WHERE Pokemon.pokemon_id = 1; --id corresponds to what pokemon is selected
 
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
