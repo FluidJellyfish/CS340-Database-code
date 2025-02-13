15c3b35b4dfdf23d92fe729bcee78e39d50175d7
Izzy Lerman <lermani@oregonstate.edu>
2025-02-12 23:03:04 -0800

    React template

---
+++ db-app/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/navbar/NavBar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
